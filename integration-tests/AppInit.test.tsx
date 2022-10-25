05143e15337e695ceab6d7d1981a56973c4b8291
Aleo <aleoy@amdigital.co.uk>
2022-10-25 19:57:30 +0100

    Created AppInit tests

--- integration-tests/AppInit.test.tsx
+++ integration-tests/AppInit.test.tsx
@@ -5,7 +5,17 @@ import App from '../src/containers/App';
 describe('App', () => {
   test('App loads correctly', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+
+    const header = screen.getByRole("heading", { name: /Capacity Counter/i });
+    expect(header).toBeVisible();
+
+    const instructions = screen.getByText(/Please provide your venue's capacity./i);
+    expect(instructions).toBeVisible();
+    
+    const capacityInput = screen.getByRole("textbox", { name: /Capacity/i });
+    expect(capacityInput).toBeVisible();
+
+    const startBtn = screen.getByRole("button", { name: /Start/i });
+    expect(startBtn).toBeVisible();
   });
 });
